5aef1a92ba8a636f3cfd5054cb7e1fd8958e8190
yubaraj poudel <yubarajpoudel708@gmail.com>
2020-03-24 13:15:07 +0545

    app bar ui fixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -31,45 +30,105 @@ class DashboardWidget extends StatefulWidget {
 
 class _DashboardWidgetState extends State<DashboardWidget> {
   List<String> selectorItems = ["National", "Province"];
+  String selectorItem = "National";
+
   _mDrawer() {
     return Drawer(
-      child: Column(children: <Widget>[
-        FlatButton.icon(onPressed: () {},
-            icon: Icon(Icons.file_upload),
-            label: Text("Upload data", style: TextStyle(color: Colors.black),))
-      ],),);
+      child: Column(
+        children: <Widget>[
+          FlatButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.file_upload),
+              label: Text(
+                "Upload data",
+                style: TextStyle(color: Colors.black),
+              ))
+        ],
+      ),
+    );
   }
 
   _statItems(String title, int count, Color color) {
-    return Card(elevation: 4.0,
+    return Card(
+      elevation: 4.0,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
       color: color,
-      child: Column( children: <Widget>[
-        Text( "$count", style: TextStyle(color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.bold),),
-        SizedBox(height: 4.0,),
-        Text(title, style: TextStyle( fontSize: 14.0),)
-      ],),);
+      child: Column(
+        children: <Widget>[
+          Text(
+            "$count",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontSize: 14.0),
+          )
+        ],
+      ),
+    );
   }
 
   _headSelector() {
-    return Row( children: <Widget>[
-      Expanded( child: Text("LATEST SITUATION REPORT", style: TextStyle( color: Colors.red, fontSize: 16.0),),),
-      SizedBox(width: 24.0,),
-      DropdownButton(
-        onChanged: (value) {
-
-      },)
-    ],);
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Text(
+            "LATEST SITUATION REPORT",
+            style: TextStyle(color: Colors.red, fontSize: 16.0),
+          ),
+        ),
+        SizedBox(
+          width: 24.0,
+        ),
+        DropdownButton(
+          items: selectorItems
+              .map<DropdownMenuItem>((string) => DropdownMenuItem<String>(
+                    value: string,
+                    child: Text(string),
+                  ))
+              .toList(),
+          value: selectorItem,
+          onChanged: (value) {
+            setState(() {
+              selectorItem = value;
+            });
+          },
+        )
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          leading: Image.asset(
+            "assets/images/corona.png",
+            height: 32.0,
+            width: 32.0,
+          ),
+          title: Text(
+            "COVID-19",
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
+          subtitle: RichText(
+            text: TextSpan(
+                text: "Case Mapping", style: TextStyle(fontSize: 14.0, color: Colors.grey), children: <TextSpan>[
+                  TextSpan( text: "NEPAL", style: TextStyle( fontSize: 14.0, color: Colors.blue))
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
-
-
-
-
