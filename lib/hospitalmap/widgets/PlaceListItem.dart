58fc742981fc2de2c72cc978376cf5cfbe2bff98
Nishon Tandukar <nishon.tan@gmail.com>
2020-03-24 22:48:12 +0545

    Adds google maps laucher

--- lib/hospitalmap/widgets/PlaceListItem.dart
+++ lib/hospitalmap/widgets/PlaceListItem.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:map_launcher/map_launcher.dart';
 import 'package:openspaces/hospitalmap/bloc/point_of_interest_bloc.dart';
 
 import '../../covid19/colors.dart';
@@ -77,10 +79,15 @@ class PlaceListItem extends StatelessWidget {
                       text: pointOfInterest.ownershipDisplay,
                       style: TextStyle(color: OpenSpaceColors.icon_color)),
                 ),
-                Icon(
-                  Icons.directions,
-                  size: 32,
-                  color: OpenSpaceColors.icon_color,
+                InkWell(
+                  onTap: () {
+                    launchInMaps(pointOfInterest);
+                  },
+                  child: Icon(
+                    Icons.directions,
+                    size: 32,
+                    color: OpenSpaceColors.icon_color,
+                  ),
                 )
               ],
             ),
@@ -88,131 +95,12 @@ class PlaceListItem extends StatelessWidget {
         ));
   }
 
-  Widget leftSection() {
-    return buildNetworkCacheImageWidget("openSpace.thumbnail", 70.0, 70.0);
-  }
-
-  Widget middleSection(BuildContext context) {
-    return Expanded(
-        child: Padding(
-      padding: const EdgeInsets.only(left: 16.0),
-      child: Container(
-        // color: Colors.black,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            middleUpperSection(context),
-            Expanded(
-              child: Container(
-                height: 10.0,
-              ),
-            ),
-            middleLowerSection(context),
-          ],
-        ),
-      ),
-    ));
-  }
-
-  Widget middleUpperSection(BuildContext context) {
-    return Container(
-      // color: Colors.blue,
-      child: Text(
-        getSafeString(pointOfInterest.name),
-        overflow: TextOverflow.ellipsis,
-        style: Theme.of(context).textTheme.title,
-      ),
+  void launchInMaps(PointOfInterest pointOfInterest) async {
+    final availableMaps = await MapLauncher.installedMaps;
+    availableMaps.first.showMarker(
+      description: "",
+      coords: Coords(pointOfInterest.lat, pointOfInterest.long),
+      title: pointOfInterest.name,
     );
-  }
-
-  Widget middleLowerSection(BuildContext buildContext) {
-    return Container(
-      // color: Colors.amber,
-      child: Row(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    size: 20.0,
-                    color: OpenSpaceColors.grey,
-                  ),
-                  Text(
-                    getSafeString("openSpace.address"),
-                    style: Theme.of(buildContext).textTheme.body1,
-                  )
-                ],
-              ),
-
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    size: 20.0,
-                    color: OpenSpaceColors.grey,
-                  ),
-                  StreamBuilder(
-                      stream: null,
-                      builder: (context, snapshot) {
-                        var text = "Not Available";
-
-//                        if (snapshot.hasData) {
-//                          text = calcApproxDistance(
-//                              snapshot.data,
-//                              LatLng(openSpace.centroid[1],
-//                                  openSpace.centroid[0]));
-//
-//                          openSpace.distanceFromCurrentLocation = text;
-//                        }
-
-                        return Text(
-                          text,
-                          style: Theme.of(buildContext).textTheme.body1,
-                        );
-                      })
-                ],
-              ),
-//              Row(
-//                children: <Widget>[
-//                  Icon(
-//                    Icons.map,
-//                    size: 20.0,
-//                  ),
-//                  Text(
-//                    "200m",
-//                    style: TextStyle(fontSize: 13.0, color: OspColors.grey),
-//                  )
-//                ],
-//              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget rightSection() {
-    return FloatingActionButton(
-        heroTag: null,
-        mini: true,
-        elevation: 0.0,
-        onPressed: () {},
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.directions,
-              size: 16.0,
-            ),
-            Text(
-              "GO",
-              style: TextStyle(fontSize: 10),
-            )
-          ],
-        ));
   }
 }
