fb9e2a59a9d73fd7f959f85976aad53da5f88f46
yubaraj poudel <yubarajpoudel708@gmail.com>
2020-03-25 14:24:23 +0545

    comming soon page added

---
+++ lib/covid19/ui/home/page_comming_soon.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CommingSoonPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Container( child: Text("Under development, will be available soon", textAlign: TextAlign.center,),));
+  }
+}
