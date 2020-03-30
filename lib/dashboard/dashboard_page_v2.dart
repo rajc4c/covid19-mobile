2482271b404775cd2c243f1a5d6a318a63c12278
Nishon Tandukar <nishon.tan@gmail.com>
2020-03-30 15:45:59 +0545

    Adds dashboard package

---
+++ lib/dashboard/dashboard_page_v2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DashBoardPageV2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Text("New content here"),
+      ),
+    );
+  }
+}
