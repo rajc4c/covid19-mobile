54ee78edd3f5e5b7e5afd024853911d7c22b10c7
Binabh Devkota <binabhdevkota@gmail.com>
2020-03-31 20:55:33 +0545

    completed

---
+++ lib/hospitalmap/widgets/covid_app_bar_faq.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:openspaces/covid19/colors.dart';
+
+AppBar covidAppBar() {
+  return AppBar(
+      backgroundColor: OpenSpaceColors.white,
+      centerTitle: true,
+      elevation: 1,
+      iconTheme: IconThemeData(color: Colors.black),
+      title: Text(
+      "प्राय सोधिने प्रश्नहरु",
+      style: TextStyle(
+          fontSize: 13.0, fontWeight: FontWeight.bold, color: Color(0xffee1919)),
+    ),);
+}
