b82776c1d350d095bd94a24b6293f5c349133605
samirdangal <samirdangal@gmail.com>
2020-04-01 12:30:16 +0545

    [#46] count in nepali number

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -47,14 +47,11 @@ class Utils {
 
   static String numberMap(String number) {
     String mappedNumber = "";
-
     Map map = {"0": "०", "1": '१', "2": '२', "3": '३', "4": '४', "5": '५', "6": '६', "7": '७', "8": '८', "9": '९'};
 
     for(int i=0; i< number.length; i++){
       String no = map[number[i]];
-
       mappedNumber = mappedNumber+no;
-
     }
     return mappedNumber;
   }
