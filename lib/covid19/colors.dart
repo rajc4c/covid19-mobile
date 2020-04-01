622bd93eb9fa6ba6023d3c601ee6d6638fe76649
samirdangal <samirdangal@gmail.com>
2020-04-01 16:28:24 +0545

    [#52] color code updated

--- lib/covid19/colors.dart
+++ lib/covid19/colors.dart
@@ -7,7 +7,7 @@ class OpenSpaceColors {
   static final Color blue = headline_blue;
   static final Color blue_transparent = headline_blue.withOpacity(0.3);
 
-  static Color headline_blue = Color(0xff007bff);
+  static Color headline_blue = Color(0xff0D49EF);
   static const Color transparent = Color(0x00000000);
   static const Color gradient_bottom = Color(0xFFFFFF);
   static var inactive_tab_text = Color(0xffBDBDBD);
@@ -15,9 +15,9 @@ class OpenSpaceColors {
   static var slider_pull_line = inactive_tab_text;
 
   static final Color grey = Colors.grey;
-  static final Color green = Color(0xff5ACE52);
+  static final Color green = Color(0xff00AD13);
   static final Color light_green = Color(0xffE8F8E7);
-  static final Color green_transparent = Color(0xff5ACE52).withOpacity(0.3);
+  static final Color green_transparent = green.withOpacity(0.3);
   static final Color white = Colors.white;
   static final Color icon_color = Color(0xff7C7C7C);
 
